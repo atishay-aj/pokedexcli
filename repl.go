2b3d628b41d079c56a4de6249130593e517eb7a5
Atishay Jain <atishayjain759@gmail.com>
2025-01-30 00:56:31 +0530

    added commands help and exit

--- repl.go
+++ repl.go
@@ -16,8 +16,19 @@ func startRepl() {
 		scanner.Scan()
 		text := scanner.Text()
 		cleaned := cleanInput(text)
-		command := cleaned[0]
-		fmt.Println("Your command was:", command)
+
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		input_command := cleaned[0]
+		availableCmds := getCommands()
+		command, ok := availableCmds[input_command]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		command.callback()
 	}
 }
 
@@ -25,4 +36,43 @@ func cleanInput(str string) []string {
 	lowered := strings.ToLower(str)
 	words := strings.Fields(lowered)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+func getCommands() map[string]cliCommand {
+
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandExit() {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+}
+
+func commandHelp() {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+
+	availableCmds := getCommands()
+	for _, cmd := range availableCmds {
+		fmt.Printf("%v: %v", cmd.name, cmd.description)
+		fmt.Println("")
+	}
 }
