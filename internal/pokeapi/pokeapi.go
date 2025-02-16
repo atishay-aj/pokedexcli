010fccb41f5d0cbadc41f38d49764bfee53d030b
Atishay Jain <atishayjain759@gmail.com>
2025-02-16 14:44:42 +0530

    cache working

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -6,16 +6,20 @@ import (
 	"io"
 	"net/http"
 	"time"
+
+	"github.com/atishay-aj/pokedexcli/internal/pokecache"
 )
 
 const baseURL = "https://pokeapi.co/api/v2"
 
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
-func NewClient() Client {
+func NewClient(cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: time.Minute,
 		},
@@ -23,35 +27,47 @@ func NewClient() Client {
 }
 
 type LocationAreaResp struct {
-	Count    int    `json:"count"`
+	Count    int     `json:"count"`
 	Next     *string `json:"next"`
-	Previous *string    `json:"previous"`
+	Previous *string `json:"previous"`
 	Results  []struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"results"`
 }
 
-func (C *Client) ListLocationAreas(pageURL *string) (LocationAreaResp, error) {
+func (c *Client) ListLocationAreas(pageURL *string) (LocationAreaResp, error) {
 	endpoint := "/location-area"
 	fullURL := baseURL + endpoint
 
 	if pageURL != nil {
 		fullURL = *pageURL
 	}
+	// check cache
+	data, ok := c.cache.Get(fullURL)
+	if ok {
+		fmt.Println("cache hit")
+		locationAreaResp := LocationAreaResp{}
+		err := json.Unmarshal(data, &locationAreaResp)
+		if err != nil {
+			return LocationAreaResp{}, err
+		}
+		return locationAreaResp, nil
+	}
+
 	req, err := http.NewRequest("GET", fullURL, nil)
 	if err != nil {
 		return LocationAreaResp{}, err
 	}
 
-	resp, err := C.httpClient.Do(req)
+	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return LocationAreaResp{}, err
 	}
 	if resp.StatusCode > 399 {
 		return LocationAreaResp{}, fmt.Errorf("bad req %v", resp.StatusCode)
 	}
-	data, err := io.ReadAll(resp.Body)
+	data, err = io.ReadAll(resp.Body)
 	if err != nil {
 		return LocationAreaResp{}, err
 	}
@@ -60,5 +76,6 @@ func (C *Client) ListLocationAreas(pageURL *string) (LocationAreaResp, error) {
 	if err != nil {
 		return LocationAreaResp{}, err
 	}
+	c.cache.Add(fullURL, data)
 	return locationAreaResp, nil
 }
